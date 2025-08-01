1e0fa5d7cf064b066f74c2647923d3e12660ddba
Rushikesh Hodade <rushikeshhodade112@gmail.com>
2025-08-01 19:08:59 +0530

    WIP before fixing Js conflict

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dummy-secret-key-for-ci-only')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["rushikeshhodade.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
